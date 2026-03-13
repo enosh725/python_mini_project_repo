bdb613633146bb06137eec25aec51d97f2852664
enosh725 <enoshangani725@gmail.com>
2026-03-13 19:38:40 +0530

    initial commit

---
+++ to_do_list.py
@@ -0,0 +1,24 @@
+tasks = []
+
+while True:
+    print("\n1.Add Task")
+    print("2.View Tasks")
+    print("3.Delete Task")
+    print("4.Exit")
+
+    choice = input("Enter choice: ")
+
+    if choice == "1":
+        task = input("Enter task: ")
+        tasks.append(task)
+
+    elif choice == "2":
+        for i, task in enumerate(tasks):
+            print(i+1, task)
+
+    elif choice == "3":
+        num = int(input("Enter task number: "))
+        tasks.pop(num-1)
+
+    elif choice == "4":
+        break
